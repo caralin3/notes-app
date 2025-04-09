1cc53cf310e39c15889ee6a2019575ef7908ab7a
Cara Steinberg <caralin3@gmail.com>
2025-04-09 11:05:25 -0400

    chore(*): add component-library package

---
+++ packages/component-library/src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import "./Button.css";
+import { ButtonProps } from "./Button.types";
+
+const Button = ({ type, text, onClick }: ButtonProps) => {
+  return (
+    <button type="button" className={`Button Button-${type}`} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
