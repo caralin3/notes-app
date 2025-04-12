240bf95de4b3acbf347213eefbf9cbb7ac04ab43
Cara Steinberg <caralin3@gmail.com>
2025-04-12 10:35:50 -0400

    chore(web): add toolpad

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,40 +1,60 @@
 import { useState } from 'react';
 
-import { Box, Tiptap, Typography } from '@notes-app/ui-library';
-
-import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg?url';
+import type { Navigation } from '@toolpad/core';
+import { ReactRouterAppProvider } from '@toolpad/core/react-router';
+import { Outlet } from 'react-router';
 
 import './App.css';
+
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    title: 'Dashboard',
+    // icon: <DashboardIcon />,
+  },
+  {
+    segment: 'orders',
+    title: 'Orders',
+    // icon: <ShoppingCartIcon />,
+  },
+];
+
+const BRANDING = {
+  title: 'My Toolpad Core App',
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Box>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </Box>
-      <Tiptap />
-      <Typography variant="h1">Vite + React</Typography>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ReactRouterAppProvider navigation={NAVIGATION} branding={BRANDING}>
+      <Outlet />
+    </ReactRouterAppProvider>
+    // <AppProvider>
+    //   <Box>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </Box>
+    //   <Typography variant="h1">Vite + React</Typography>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </AppProvider>
   );
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,52 @@
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 import './index.css';
 import App from './App.tsx';
+import { Layout } from './layouts/Dashboard.tsx';
+
+// const router = createBrowserRouter([
+//   {
+//     Component: App, // root layout route
+//     children: [
+//       {
+//         path: '/',
+//         Component: Layout,
+//         children: [
+//           {
+//             path: '',
+//             Component: DashboardPage,
+//           },
+//           {
+//             path: 'orders',
+//             Component: OrdersPage,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter([
+  {
+    Component: App,
+    children: [
+      {
+        path: '/',
+        Component: Layout,
+      },
+      {
+        path: '/orders',
+        Component: Layout,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
